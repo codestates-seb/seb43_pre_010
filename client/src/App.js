237db12c17406bb1fc40eb946ea1de05adca2985
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-04-12 00:42:48 +0900

    feat: 메인 home페이지 생성과 라우팅 경로 설정 페이징 처리를 위한 home.js 생성 후 path경로를 기본 경로로 설정해서 App컴포넌트와 연결시켜둠

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
 import Questions from "./pages/questions";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/questions/:questionId" element={<Questions />} />
       </Routes>
     </BrowserRouter>
