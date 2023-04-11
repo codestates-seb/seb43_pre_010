36395e13733506c43c6814bb5273bd8203d89287
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-04-12 00:10:31 +0900

    fix: createGlobalStyle 컴포넌트의 위치를 index로 이동 createGlobalStyle는 자식 요소를 가질 수 없기때문에 App에서 자식 컴포넌트를 감싸주던 방식을 index의 App상단으로 이동시키고, 단독으로 사용하는 방식으로 수정함

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Questions from "./pages/questions";
-import GlobalStyle from "./globalStyle";
 
 const App = () => {
   return (
-    <GlobalStyle>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/questions/:questionId" element={<Questions />} />
-        </Routes>
-      </BrowserRouter>
-    </GlobalStyle>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/questions/:questionId" element={<Questions />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
