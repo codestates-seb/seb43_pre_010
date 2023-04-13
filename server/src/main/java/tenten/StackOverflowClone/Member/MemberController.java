6613bddb43fa13b3dd95ba7bd9fa15ea28d6dd1d
YunGeunSang <120304078+YunGeunSang@users.noreply.github.com>
2023-04-13 13:57:44 +0900

    test: 테스트 코드 작성

---
+++ server/src/main/java/tenten/StackOverflowClone/Member/MemberController.java
@@ -0,0 +1,11 @@
+package tenten.StackOverflowClone.Member;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
