3a6fe3edc2a6a5bda389b09a0950d09b5e59ded8
[KwonPyo Hong] <[pyo9501@gmail.com]>
2023-04-13 13:39:43 +0900

    init: Spring Initializr를 통해 서버 초기 세팅

---
+++ server/src/main/java/tenten/StackOverflowClone/StackOverflowCloneApplication.java
@@ -0,0 +1,13 @@
+package tenten.StackOverflowClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverflowCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackOverflowCloneApplication.class, args);
+	}
+
+}
