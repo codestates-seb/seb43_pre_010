5ab2294642c67aaeff5ea5c29e06954f5b70247b
YunGeunSang <120304078+YunGeunSang@users.noreply.github.com>
2023-04-18 18:01:50 +0900

    refactor: answer 리팩터링

--- server/src/main/java/tenten/StackOverflowClone/answer/controller/AnswerController.java
+++ server/src/main/java/tenten/StackOverflowClone/answer/controller/AnswerController.java
@@ -7,6 +7,7 @@ import tenten.StackOverflowClone.answer.dto.AnswerDto;
 import tenten.StackOverflowClone.answer.entity.Answer;
 import tenten.StackOverflowClone.answer.mapper.AnswerMapper;
 import tenten.StackOverflowClone.answer.service.AnswerService;
+import tenten.StackOverflowClone.dto.SingleResponseDto;
 import tenten.StackOverflowClone.utils.UriCreator;
 
 import java.net.URI;
@@ -28,7 +29,7 @@ public class AnswerController {
     @PostMapping("/{question-id}/answers")
     public ResponseEntity postAnswer(@PathVariable("question-id") long questionId,
                                      @RequestBody AnswerDto.Post requestBody){
-        // 회원 확인 로직 추가
+        // client 에서 user 정보를 따로 받기
 
         requestBody.addQuestionId(questionId);
         // requestBody.addUserId(userId);
@@ -49,7 +50,8 @@ public class AnswerController {
         Answer answer = answerMapper.answerPatchDtoToAnswer(requestBody);
         Answer updatedAnswer = answerService.updateAnswer(answer);
 
-        return ResponseEntity.ok().body(answerMapper.answerToAnswerResponseDto(updatedAnswer));
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(answerMapper.answerToAnswerResponseDto(updatedAnswer)), HttpStatus.OK);
     }
 
     @DeleteMapping("/{answer-id}")
@@ -57,14 +59,13 @@ public class AnswerController {
 
         answerService.deleteAnswer(answerId);
 
-        return ResponseEntity.noContent().build();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     // TODO: 추후 QuestionController로 이동
     @PostMapping("/{question-id}/answer/{answer-id}/like")
     public void likeAnswer(@PathVariable("question-id") long questionId,
                                      @PathVariable("answer-id") long answerId){
-
         // 질문 검증
         Answer answer = answerService.findVerifiedAnswer(answerId);
 
--- server/src/main/java/tenten/StackOverflowClone/answer/mapper/AnswerMapper.java
+++ server/src/main/java/tenten/StackOverflowClone/answer/mapper/AnswerMapper.java
@@ -12,11 +12,27 @@ import java.util.List;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AnswerMapper {
 
+    // source는 파라미터로 받는 DTO의 필드명, target은 DB의 테이블과 (엔티티 클래스)필드명을 의미한다.
+    //@Mapping(source = "questionId", target="question.id")
+    //@Mapping(source = "userid", target = "user.id")
     Answer answerPostDtoToAnswer(AnswerDto.Post requestBody);
+
     Answer answerPatchDtoToAnswer(AnswerDto.Patch requestBody);
 
     AnswerDto.Response answerToAnswerResponseDto(Answer answer);
 
+    // static으로 선언 -> 수동으로 구현
     List<AnswerDto.Response> answersToAnswerResponseDtos(List<Answer> answers);
+
+//    static AnswerDto.Response toAnswerResponseDto(Answer answer){
+//        return AnswerDto.Response.builder()
+//                .answerId(answer.getId())
+//                .content(answer.getContent())
+//                .answerUser(answer.getUser().getEmail()) // 작성자명
+//                .answerVote(answer.getScoreCount()) // 좋아요 수
+//                .build();
+//    }
+
+
 
 }
