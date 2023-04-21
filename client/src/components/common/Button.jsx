ed833a6ca61a5606da6dc0f3abac7145b6c28f85
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-04-21 09:51:23 +0900

    refactor: 공통으로 사용되는 버튼 컴포넌트 분리 버튼의 type을 지정해서 각각의 스타일링을 변경할 수 있도록 설정함 props로 전달받는 상태값과 텍스트 추가

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -1,11 +1,65 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const Button = () => {
+const Button = ({ type, icon, text }) => {
   return (
-    <div>
-      버튼
-    </div>
+    <LoginButton type={type}>
+      { icon }
+      <p>{text}</p>
+    </LoginButton>
   )
 }
 
 export default Button;
+
+const defaultStyle = css`
+  background-color: #0a95ff;
+  color: #ffffff;
+`;
+
+const googleBtnStyle = css`
+  background-color: #ffffff;
+  color: #3b4045;
+`;
+
+const gitHubBtnStyle = css`
+  background-color: #2f3337;
+  color: #ffffff;  
+`;
+
+const facebookBtnStyle = css`
+  background-color: #385499;
+  color: #ffffff;  
+`;
+
+const LoginButton = styled.button`
+  width: 100%;
+  height: 37.78px;
+  background-color: aqua;
+  margin: 4px 0px;
+  border-radius: 5px;
+  font-size: 13px;
+  font-weight: 400;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid rgb(214, 217, 220);
+
+  ${( { type } ) => {
+    switch (type) {
+      case 'google' :
+        return googleBtnStyle;
+      case 'github' :
+        return gitHubBtnStyle;
+      case 'facebook' :
+        return facebookBtnStyle;
+      default :
+        return defaultStyle;
+    }
+  }}
+
+  > p {
+    margin-left: 5px;
+    line-height: 50%;
+  }
+`;
