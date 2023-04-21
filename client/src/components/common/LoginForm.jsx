387973fcff25a5cb0e84d9353e8555bad2b1c3fa
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-04-22 03:52:57 +0900

    refactor: 소셜 로그인 버튼 상속 변경 로그인, 회원가입 페이지에 공통으로 사용되는 소셜 로그인 컴포넌트를 유저인증폼에 합침

--- client/src/components/common/LoginForm.jsx
+++ client/src/components/common/LoginForm.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import styled from "styled-components";
 import Button from "./Button";
-import { SignUpHelpIcon, SignUpTalentIcon } from "../../assets/Login/LoginSVG";
+import { SignUpHelpIcon, SignUpTalentIcon, GoogleIcon, GithubIcon, FacebookIcon } from "../../assets/Login/LoginSVG";
+import PrivacyImg from "../../assets/Login/PrivacyImg.png";
+import { SOLogoSvg } from "../../assets/Header/HeaderSVG";
 
 const LoginForm = ({ isSignup }) => {
 
   return (
-    <LoginFormWrapper>
+    <LoginFormWrapper isSignup={isSignup}>
+        { !isSignup && <SOLogoSvg />}
+        <SocialLogins isSignup={isSignup}>
+          <Button type="google" icon={<GoogleIcon />} text={`${isSignup ? "Sign up" : "Log in"} with Google`} />
+          <Button type="github" icon={<GithubIcon />} text={`${isSignup ? "Sign up" : "Log in"} with GitHub`} />
+          <Button type="facebook" icon={<FacebookIcon />} text={`${isSignup ? "Sign up" : "Log in"} with Facebook`} />
+        </SocialLogins>
       <LoginFormContent>
         <form>
           {
@@ -22,38 +30,47 @@ const LoginForm = ({ isSignup }) => {
             <input type="email" name="email" id="email"/>
           </label>
           <label htmlFor="password">
-            <h4>Password</h4>
-            <h4>forgot password?</h4>
+            <div className="password-help-text">
+              <h4>Password</h4>
+              { !isSignup && <h4 className="forgot-text">Forgot password?</h4> }
+            </div>
             <input type="password" name="password" id="password" />
-            { isSignup && <p>Passwords must contain at least eight characters,<br />including at least 1 letter and 1 number.</p>}
+            { isSignup && <p className="sub-text passwords-text">Passwords must contain at least eight characters,<br />including at least 1 letter and 1 number.</p>}
           </label>
+          { isSignup && <PrivacyContent />}
           {
             isSignup && (
-              <label htmlFor="check">
-                <input type="checkbox" id="check" />
+
+              <label htmlFor="check" className="check-form">
+                <div className="check-box">
+                  <input type="checkbox" id="check" />
+                </div>
                 <p>Opt-in to receive occasional product<br />updates, user research invitations, company<br />announcements, and digests.</p>
                 <SignUpHelpIcon />
               </label>
+
             )
           }
             <Button text={isSignup ? "Sign up" : "Log in"} />
             {
               isSignup && (
-                <p>
+                <p className="sub-text">
                   By clicking “Sign up”, you agree to our
-                  <span>terms of<br />service</span>
+                  <span> terms of<br />service</span>
                   , <span>privacy policy</span> and <span>cookie policy</span>
                 </p>
               )
             }
         </form>
-        <p>
+        <p className="bottom-text">
           { isSignup ? "Already have an account?" : "Don't have an account?"}
           <button type="button" >{isSignup ? "Log in" : "Sign up"}</button>
         </p>
-        <p>Are you an employer?</p>
-        <button type="button" >Sign up on Talent</button>
-        <SignUpTalentIcon />
+        <div className="employer-help ">
+          <p>Are you an employer?</p>
+          <button type="button" >Sign up on Talent</button>
+          <SignUpTalentIcon />
+        </div>
       </LoginFormContent>
     </LoginFormWrapper>
   );
@@ -62,10 +79,136 @@ const LoginForm = ({ isSignup }) => {
 export default LoginForm;
 
 const LoginFormWrapper = styled.section`
-  
+  width: ${({ isSignup }) => isSignup ? "316px" : "278px" };
+
+  .iconLogoGlyphMd {
+    margin: 0 auto;
+    margin-bottom: 24px;
+    cursor: pointer;
+  }
 `;
+
+const SocialLogins = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: ${({ isSignup }) => isSignup ? "316px" : "278px" };
+  margin-bottom: 16px;
+`;
+
+const PrivacyContent = styled.div`
+  background-image: url(${PrivacyImg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 267px;
+  height: 153px;
+  margin: 10px 0px;
+  border: 1px solid rgb(227, 230, 232);
+  border-radius: 3px;
+`
 
 const LoginFormContent = styled.div`
-  
-`;
+  .password-help-text {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-align: center;
+  }
+  .forgot-text {
+    color: rgb(0, 116, 204);
+    font-size: 12px;
+    cursor: pointer;
+  }
+  .forgot-text:hover {
+    color: hsl(206, 100%, 52%);
+  }
+  p {
+    font-size: 12px;
+  }
+  span {
+    font-size: 12px;
+    color: #007ac6;
+  }
+  button {
+    font-size: 12px;
+    color: #0074cc;
+    padding: 0px 5px;
+  }
+  label {
+    font-size: 15px;
+    font-weight: 600;
+    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI Adjusted","Segoe UI","Liberation Sans",sans-serif;
+    color: rgb(12, 13, 14);
+  }
+  form {
+    margin-bottom: 24px;
+    padding: 22px;
+    background-color: rgb(255, 255, 255);
+    box-shadow: rgba(0, 0, 0, 0.05) 0px 10px 24px 0px, rgba(0, 0, 0, 0.05) 0px 20px 48px 0px, rgba(0, 0, 0, 0.1) 0px 1px 4px 0px;
+    border-radius: 7px;
+  }
+  input {
+    min-width: 100%;
+    padding: 7.8px 9px;
+    font-size: 13px;
+    border: 1px solid #0000003e;
+    border-radius: 3px;
+    font-weight: 400;
+    margin: 6px 0px 13px 0px;
+  }
+  input:focus {
+    outline: none;
+    border-color: hsl(206, 90%, 69.5%);
+    box-shadow: 0 0 0 4px hsla(206, 100%, 40%, .15);
+  }
+  .sub-text {
+    color: rgb(106, 115, 124);
+    font-size: 12px;
+    font-weight: 400;
+    margin-top: 28px;
+  }
+  .passwords-text {
+    margin-top: -10px;
+    margin-bottom: 15px;
+  }
 
+  .check-form {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: row;
+    margin-bottom: 15px;
+    > p {
+      font-size: 12px;
+      font-weight: 400;
+    }
+  }
+  .check-box {
+    width: 13px;
+    height: 13px;
+    display: flex;
+    align-items: center;
+    padding-bottom: 30px;
+    margin-right: 4px;
+  }
+  .iconHelpSm {
+    width: 14px;
+    height: 14px;
+    margin-bottom: 35px;
+  }
+
+  .employer-help {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin: 15px 0px;
+  }
+
+  .iconShareSm {
+    margin-top: 5px;
+  }
+
+  .bottom-text {
+    text-align: center;
+  }
+`;
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import styled, { css } from "styled-components";
 import HeaderBar from "../components/common/header/HeaderBar";
-import Button from "../components/common/Button";
 import LoginForm from "../components/common/LoginForm";
-import { SOLogoSvg } from "../assets/Header/HeaderSVG";
-import { GoogleIcon, GithubIcon, FacebookIcon } from "../assets/Login/LoginSVG";
+import AboutSignUp from "../components/AboutSignUp";
 
 const LoginPage = () => {
 
@@ -13,15 +11,10 @@ const LoginPage = () => {
   return (
   <>
     <HeaderBar />
-    <LoginPageWrapper>
+    <LoginPageWrapper isSignup={isSignup}>
       <LoginContent>
-        <SOLogoSvg />
-        <SocialLogins>
-          <Button type="google" icon={<GoogleIcon />} text={`${isSignup ? "Sign up" : "Log in"} with Google`} />
-          <Button type="github" icon={<GithubIcon />} text={`${isSignup ? "Sign up" : "Log in"} with GitHub`} />
-          <Button type="facebook" icon={<FacebookIcon />} text={`${isSignup ? "Sign up" : "Log in"} with Facebook`} />
-        </SocialLogins>
-          <LoginForm isSignup={isSignup} />
+        { isSignup && <AboutSignUp />}
+        <LoginForm isSignup={isSignup} />
       </LoginContent>
     </LoginPageWrapper>
   </>
@@ -37,29 +30,19 @@ const LoginPageWrapper = styled.div`
   justify-content: center;
   background-color: rgb(241, 242, 243);
   position: relative;
+  flex-direction: row;
+  align-items: center;
+  background-attachment: scroll;
+  height: ${({ isSignup }) => isSignup ? "100%" : "calc(100vh - 50px)" };
+  min-height: ${({ isSignup }) => !isSignup && "600px" };
 `;
 
 const LoginContent = styled.div`
   width: 100%;
   max-width: 1264px;
   margin: 0;
-  background-color: transparent;
   padding: calc(24px * 1);
   display: flex;
   align-items: center;
   justify-content: center;
-  height: calc(100vh - 50px);
-  flex-direction: column;
-
-  .iconLogoGlyphMd {
-    margin-bottom: 24px;
-    cursor: pointer;
-  }
-`;
-
-const SocialLogins = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 278px;
-  margin-bottom: 16px;
 `;
