9a77619c28426e3abf0378dd78ab90b54477b5ba
hongkwonpyo <119943253+hongkwonpyo@users.noreply.github.com>
2023-04-21 16:55:09 +0900

    Revert "second commit"

--- server/src/main/java/tenten/StackOverflowClone/config/SecurityConfig.java
+++ server/src/main/java/tenten/StackOverflowClone/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,20 +12,20 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests() // 요청에 의한 보안검사 시작
-                .anyRequest().authenticated() //어떤 요청에도 보안검사를 한다.
-                .and()
-                .formLogin();//보안 검증은 formLogin방식으로 하겠다.
-    }
+public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+                .csrf().disable()
+                .authorizeRequests(authorize -> authorize
+                        .anyRequest().permitAll());
+
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
-
