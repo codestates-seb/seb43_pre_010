c0fa1edc8299b843bd69a8af6978c0d68fcadada
[KwonPyo Hong] <[pyo9501@gmail.com]>
2023-04-25 10:10:13 +0900

    feat: security config 작성

--- server/src/main/java/tenten/StackOverflowClone/config/SecurityConfig.java
+++ server/src/main/java/tenten/StackOverflowClone/config/SecurityConfig.java
@@ -2,7 +2,11 @@ package tenten.StackOverflowClone.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -10,14 +14,35 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfig {
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
         http
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .cors()
+                .and()
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+//                .apply(new CustomFilterConfigurer())
+//                .and()
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll());
+                        .antMatchers(HttpMethod.OPTIONS).permitAll()
+                        .antMatchers(HttpMethod.GET).permitAll()
+                        .antMatchers(HttpMethod.POST,"/members/join").permitAll()
+                        .antMatchers(HttpMethod.PATCH,"/pwchange/registeremail").permitAll()
+                        .antMatchers(HttpMethod.POST,"/pwchange").permitAll()
+                        .anyRequest().authenticated()
+                );
 
         return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
--- server/src/main/java/tenten/StackOverflowClone/user/service/UserService.java
+++ server/src/main/java/tenten/StackOverflowClone/user/service/UserService.java
@@ -4,9 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import tenten.StackOverflowClone.exception.BusinessLogicException;
+import tenten.StackOverflowClone.exception.ExceptionCode;
 import tenten.StackOverflowClone.user.dto.UserResponseDto;
 import tenten.StackOverflowClone.user.entity.User;
 import tenten.StackOverflowClone.user.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -20,9 +25,59 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-//    public User creatUser(User user) {
-//        String encryptedPassword = passwordEncoder.encode(user.getPassword());
-//        user.setPassword(encryptedPassword);
-//    }
+    public User createUser(User user) {
+        //이메일 중복 확인
+        verifyExistsEmail(user.getEmail());
+        //비밀번호 암호화
+        String encryptedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encryptedPassword);
+        //userStatus 설정
+        user.setUserStatus(User.UserStatus.USER_ACTIVE);
+
+//        List<String> roles = authori
+        //회원가입
+        userRepository.save(user);
+
+
+        return user;
+    }
+
+    public User updateUser(User user) {
+        User findUser = findVerifiedUser(user.getUserId());
+
+        Optional.ofNullable(user.getName())
+                .ifPresent(name->findUser.setName(name));
+
+        return userRepository.save(findUser);
+    }
+
+    public User findUser(long userId) {
+        User findUser = findVerifiedUser(userId);
+        return findUser;
+    }
+
+    public void deleteUser(User user) {
+        user.setUserStatus(User.UserStatus.USER_QUIT);
+        userRepository.save(user);
+    }
+    public void verifyExistsEmail(String email) {
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isPresent()) {
+            throw new BusinessLogicException(ExceptionCode.USER_EXIST);
+        }
+    }
+    public User findVerifiedUser(Long userId) {
+        Optional<User> user = userRepository.findById(userId);
+        User findUser = user.orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
+        return findUser;
+    }
+
+    public User findByEmail(String email) {
+        Optional<User> user = userRepository.findByEmail(email);
+        User findUser = user.orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
+        return findUser;
+    }
+
+//    public void aut
 
 }
