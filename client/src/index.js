a2e6a1e9981a9137a759a0fccd276bd6e05e4fa1
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-04-26 07:30:43 +0900

    feat: 로그인 된 유저정보를 전역적으로 사용할 수 있도록 리덕스툴킷을 이용 로그인, 로그아웃 기능과 유저 프로필 정보를 가져올 수 있는 상태관리 함수 추가

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from 'react-redux';
+import store from './store/store';
 import App from "./App";
 import GlobalStyle from "./globalStyle";
 import "./index.css";
@@ -7,7 +9,9 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
